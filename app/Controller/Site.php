28876944fb86fa1add44bcfd634da8cee4f1c890
AnastasiaKilko <anastasia.v.kilko@gmail.com>
2025-11-13 17:55:31 +0700

    auth ready p 57

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -6,6 +6,8 @@ use Model\Post;
 use Model\User;
 use Src\View;
 use Src\Request;
+use Src\Auth\Auth;
+
 class Site
 {
     public function index(Request $request): string
@@ -21,9 +23,25 @@ class Site
 
     public function signup(Request $request): string
     {
-        if ($request->method==='POST' && User::create($request->all())){
+        if ($request->method === 'POST' && User::create($request->all())) {
             app()->route->redirect('/go');
         }
         return new View('site.signup');
+    }
+
+    public function login(Request $request): string
+    {
+        if ($request->method === 'GET') {
+            return new View('site.login');
+        }
+        if (Auth::attempt($request->all())) {
+            app()->route->redirect('/hello');
+        }
+        return new View('site.login', ['message' => 'Неправильные логин или пароль']);
+    }
+
+    public function logout(): void {
+        Auth::logout();
+        app()->route->redirect('/hello');
     }
 }
